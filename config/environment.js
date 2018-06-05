ac759adc647850483f8de4ee79075ffa8141df5b
Nick Steffens <nick@popularpays.com>
2018-06-05 14:26:36 -0400

    Fixing Logo

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,14 @@
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+module.exports = function(environment) {
+  let ENV = {
+    modulePrefix: 'ember-cli-popularpays-styleguide',
+    // namespaced directory where resolver will look for your resource files
+    podModulePrefix: 'ember-cli-popularpays-styleguide/pods',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto'
+  };
+
+  return ENV;
 };
